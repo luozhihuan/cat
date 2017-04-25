bd15d9301ad586a36fa5c9ff61edc280eae251d5
luozhihuan <luozhihuan@meituan.com>
2017-04-25 15:10:06 +0800

    删除target目录

--- cat-home/src/main/java/com/dianping/cat/CatHomeModule.java
+++ cat-home/src/main/java/com/dianping/cat/CatHomeModule.java
@@ -1,18 +1,9 @@
 package com.dianping.cat;
 
-import java.io.File;
-
-import org.unidal.helper.Threads;
-import org.unidal.initialization.AbstractModule;
-import org.unidal.initialization.Module;
-import org.unidal.initialization.ModuleContext;
-
 import com.dianping.cat.analysis.MessageConsumer;
 import com.dianping.cat.analysis.TcpSocketReceiver;
 import com.dianping.cat.config.server.ServerConfigManager;
 import com.dianping.cat.consumer.CatConsumerModule;
-import com.dianping.cat.report.page.ConfigReloadTask;
-import com.dianping.cat.report.task.DefaultTaskConsumer;
 import com.dianping.cat.report.alert.app.AppAlert;
 import com.dianping.cat.report.alert.business.BusinessAlert;
 import com.dianping.cat.report.alert.database.DatabaseAlert;
@@ -28,6 +19,14 @@ import com.dianping.cat.report.alert.thirdParty.ThirdPartyAlert;
 import com.dianping.cat.report.alert.thirdParty.ThirdPartyAlertBuilder;
 import com.dianping.cat.report.alert.transaction.TransactionAlert;
 import com.dianping.cat.report.alert.web.WebAlert;
+import com.dianping.cat.report.page.ConfigReloadTask;
+import com.dianping.cat.report.task.DefaultTaskConsumer;
+import org.unidal.helper.Threads;
+import org.unidal.initialization.AbstractModule;
+import org.unidal.initialization.Module;
+import org.unidal.initialization.ModuleContext;
+
+import java.io.File;
 
 public class CatHomeModule extends AbstractModule {
 	public static final String ID = "cat-home";
@@ -113,5 +112,9 @@ public class CatHomeModule extends AbstractModule {
 			}
 		});
 	}
+
+//	public static void main(String[] args) {
+//		new GenerateContext();
+//	}
 
 }
