5bf23ed8a4a4fe8fbe65cb3ba3e2503f11df2fc6
luozhihuan <luozhihuan@meituan.com>
2017-05-09 09:46:36 +0800

    发布脚本

--- cat-home/deploy/online/run.sh
+++ cat-home/deploy/online/run.sh
@@ -23,9 +23,9 @@ function monitorinit(){
     clientxml="$MONITOR_PATH/client.xml"
     datasourcesxml="/data/appdatas/datasources.xml"
     serverxml="/data/appdatas/server.xml"
-    cat online/client.xml > $clientxml
-    cat online/datasources.xml > $datasourcesxml
-    cat online/server.xml > $serverxml
+    cat deploy/online/client.xml > $clientxml
+    cat deploy/online/datasources.xml > $datasourcesxml
+    cat deploy/online/server.xml > $serverxml
 }
 
 function init() {
